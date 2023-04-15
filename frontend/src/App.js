38a025a8fc3f37bb59f8415448ad8df97322a647
Ibrahim Saidi <ibrahim.aka.ajax@gmail.com>
2023-04-15 15:04:23 +0930

    Frontend introduced ⚠️

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import LoginForm from './LoginForm';
+
+function App() {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/LoginForm.js
@@ -0,0 +1,63 @@
+// src/LoginForm.js
+import React, { useState } from 'react';
+import { Button, TextField, Container, Typography } from '@mui/material';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:8000/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Login failed');
+      }
+
+      const data = await response.json();
+      localStorage.setItem('access_token', data.access_token);
+      alert('Login successful');
+    } catch (error) {
+      alert('Login failed');
+    }
+  };
+
+  return (
+    <Container>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Login
+      </Typography>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          label="Username"
+          variant="outlined"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          fullWidth
+          margin="normal"
+        />
+        <TextField
+          label="Password"
+          type="password"
+          variant="outlined"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          fullWidth
+          margin="normal"
+        />
+        <Button type="submit" variant="contained" color="primary">
+          Login
+        </Button>
+      </form>
+    </Container>
+  );
+};
+
+export default LoginForm;
