18190bd3f9c09e3adde12416781d2462551ac4b0
Ibrahim Saidi <ibrahim.aka.ajax@gmail.com>
2023-05-03 08:28:27 +0930

    Project structure 🚀

---
+++ frontend/src/theme/theme.ts
@@ -0,0 +1,31 @@
+// src/theme/theme.ts
+
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#3f51b5',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+    background: {
+      default: '#f4f4f4',
+    },
+  },
+  typography: {
+    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none',
+        },
+      },
+    },
+  },
+});
+
+export default theme;
